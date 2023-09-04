76d06cc59c906794b3b8711c6a92c32d5198ed2a
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-04 16:47:28 +0300

    Create afiş.js

---
+++ slashcmds/utility/afiş.js
@@ -0,0 +1,58 @@
+const {SlashCommandBuilder} = require(`discord.js`);
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName(`afiş`)
+  .setDescription(`Bir üyenin veya sunucunun afişini gönderir.`)
+  .addSubcommand(subcommand => subcommand
+    .setName(`üye`)
+    .setDescription(`Sizin veya seçtiğiniz üyenin afişini gönderir.`)
+    .addUserOption(option => option
+      .setName(`üye`)
+      .setDescription(`Afişini görmek istediğiniz üyeyi seçin.`)
+      .setRequired(false)
+    )
+  )
+  .addSubcommand(subcommand => subcommand
+    .setName(`sunucu`)
+    .setDescription(`Bulunduğunuz sunucunun afişini gönderir.`)
+  ),
+
+  run: async (client, interaction) => {
+
+  if (interaction.options.getSubcommand() == `üye`) {
+    const {EmbedBuilder} = require(`discord.js`),
+    member = interaction.options.getUser(`üye`) || interaction.member.user;
+
+    if (member.banner) {
+      let embed = new EmbedBuilder()
+      .setColor(`#00FFB8`)
+      .setTitle(`${member.username} Kullanıcısının Afişi`)
+  	  .setImage(`https://cdn.discordapp.com/banners/${member.id}/${member.banner}.png?size=1024`)
+      .setDescription(`**gif** [[512]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.gif?size=512) [[1024]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.gif?size=1024) [[2048]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.gif?size=2048)\n**jpg** [[512]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.jpg?size=512) [[1024]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.jpg?size=1024) [[2048]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.jpg?size=2048)\n**png** [[512]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.png?size=512) [[1024]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.png?size=1024) [[2048]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.png?size=2048)\n**webp** [[512]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.webp?size=512) [[1024]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.webp?size=1024) [[2048]](https://cdn.discordapp.com/banners/${member.id}/${member.banner}.webp?size=2048)`);
+      await interaction.followUp({embeds: [embed]});
+
+    } else {
+      await interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu kullanıcının bir afişi yok!`, ephemeral: true});
+    };
+  };
+
+  if (interaction.options.getSubcommand() == `sunucu`) {
+    const {EmbedBuilder} = require(`discord.js`),
+    guild = interaction.guild;
+
+    if (interaction.guild.banner) {
+      let embed = new EmbedBuilder()
+      .setColor(`#00FFB8`)
+      .setTitle(`${guild.name} Sunucusunun Afişi`)
+	    .setImage(`https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.png?size=1024`)
+      .setDescription(`**gif** [[512]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.gif?size=512) [[1024]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.gif?size=1024) [[2048]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.gif?size=2048)\n**jpg** [[512]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.jpg?size=512) [[1024]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.jpg?size=1024) [[2048]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.jpg?size=2048)\n**png** [[512]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.png?size=512) [[1024]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.png?size=1024) [[2048]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.png?size=2048)\n**webp** [[512]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.webp?size=512) [[1024]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.webp?size=1024) [[2048]](https://cdn.discordapp.com/banners/${guild.id}/${guild.banner}.webp?size=2048)`);
+      await interaction.followUp({embeds: [embed]});
+
+    } else {
+      await interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu sunucunun bir afişi yok!`, ephemeral: true});
+    };
+  };
+
+  },
+};
