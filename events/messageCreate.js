d176a971b2253ff2f068ae82257be9e42da24eea
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-04 16:08:18 +0300

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,15 @@
+module.exports = {
+  run: async (client, message) => {
+
+  if (message.author.bot || !message.guild || !message.content.startsWith(client.settings.config.prefix)) return;
+  const args = message.content.slice(client.settings.config.prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  let command = client.prefixcmds.get(cmd);
+  if (!command) command = client.prefixcmds.get(client.aliases.get(cmd));
+  if (!command) return;
+
+  command.run(client, message, args);
+
+  },
+};
