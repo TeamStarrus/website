a3758a65ab98a16022d1d18bf34de11321ac9681
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-04 16:44:49 +0300

    Create çay-iç.js

---
+++ slashcmds/fun/çay-iç.js
@@ -0,0 +1,20 @@
+const {SlashCommandBuilder} = require(`discord.js`);
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName(`çay-iç`)
+  .setDescription(`Boğaz manzarası karşısında çay içersiniz.`),
+
+  run: async (client, interaction) => {
+
+  const {EmbedBuilder} = require(`discord.js`);
+
+  let embed = new EmbedBuilder()
+  .setColor(`#00FFB8`)
+  .setImage(`https://i.hizliresim.com/4ey00j0.jpg`)
+  .setTitle(`${interaction.member.user.username} çay içti!`);
+
+  await interaction.followUp({embeds: [embed]});
+
+  },
+};
