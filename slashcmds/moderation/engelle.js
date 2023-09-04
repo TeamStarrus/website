bda1016209767bdd4aa4b9df806ef64b733d40fe
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-04 16:45:47 +0300

    Create engelle.js

---
+++ slashcmds/moderation/engelle.js
@@ -0,0 +1,42 @@
+const {SlashCommandBuilder} = require(`discord.js`);
+
+module.exports = {
+  data: new SlashCommandBuilder()
+  .setName(`engelle`)
+  .setDescription(`Belirtilen üyeyi sunucudan engeller.`)
+  .addUserOption(option => option
+    .setName(`üye`)
+    .setDescription(`Engellemek istediğin üyeyi seç.`)
+    .setRequired(true)
+  )
+  .addStringOption(option => option
+    .setName(`sebep`)
+    .setDescription(`Üyeyi engelleme sebebini yaz.`)
+    .setRequired(false)
+  ),
+
+  run: async (client, interaction) => {
+
+	if (!interaction.member.permissions.has(`BAN_MEMBERS`)) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu komutu kullanabilmek için \`Üyeleri Engelle\` iznine sahip olmalısın!`, ephemeral: true});
+
+  const user = interaction.options.getUser(`üye`),
+  member = interaction.guild.members.cache.get(user.id) || await interaction.guild.members.fetch(user.id).catch(err => {});
+
+  if (!member) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Sunucuda böyle bir üye bulunamadı.`, ephemeral: true});
+
+  if (!member.bannable || member.user.id == client.user.id || interaction.member.roles.highest.position <= member.roles.highest.position) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu üyeyi sunucudan engelleyemiyorum.`, ephemeral: true});
+
+  const reason = interaction.options.getString(`sebep`);
+  if (reason) {
+    await interaction.followUp({content: `${client.settings.config.emotes.greenTick} ${member.user.tag} adlı kullanıcı \`${reason}\` sebebiyle sunucudan engellendi!`});
+    await member.user.send({content: `**\`${interaction.guild.name}\`** sunucusundan \`${reason}\` sebebiyle engellendin!`}).catch(err => {});
+    await member.ban({reason});
+
+  } else {
+    await interaction.followUp({content: `${client.settings.config.emotes.greenTick} ${member.user.tag} adlı kullanıcı sunucudan engellendi! Sebep belirtilmedi.`});
+    await member.user.send({content: `**\`${interaction.guild.name}\`** sunucusundan engellendin! Sebep belirtilmedi.`}).catch(err => {});
+    await member.ban();
+  };
+
+  },
+};
