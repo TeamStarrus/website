24e774463b79a45698e7140c22311237a9cd5813
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-04 16:07:52 +0300

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,34 @@
+module.exports = {
+  run: async (client, interaction) => {
+
+  const InteractionType = require(`discord.js`);
+
+  if (!interaction.type == InteractionType.ApplicationCommand || interaction.member.bot || !interaction.guild) return;
+  await interaction.deferReply().catch(err => {});
+
+  const {commandName} = interaction,
+  command = client.slashcmds.get(commandName);
+
+  const {ActionRowBuilder, ButtonBuilder, ButtonStyle} = require(`discord.js`);
+
+  let row = new ActionRowBuilder()
+  .addComponents(
+    new ButtonBuilder()
+    .setStyle(ButtonStyle.Link)
+    .setLabel(`Destek sunucusuna katıl!`)
+    .setURL(`https://discord.com/invite/${client.settings.config.server}`)
+  );
+
+  let errorMessage = `${client.settings.config.emotes.redTick} Komut yürütülürken bir hata oluştu! `, errorMessage2 = `\nLütfen bu hatayı destek ekibimize bildiriniz.`
+
+  if (!command) return interaction.followUp({content: errorMessage + `\`(Command not found.)\`` + errorMessage2, components: [row], ephemeral: true});
+
+  try {
+    if (command) await command.run(client, interaction);
+  } catch (err) {
+    await console.log(err);
+    return interaction.followUp({content: errorMessage + `\`(Command gave an error.)\`` + errorMessage2, components: [row], ephemeral: true})
+  };
+
+  },
+};
