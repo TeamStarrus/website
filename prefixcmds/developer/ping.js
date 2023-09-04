e5e50f2b7b444cc332972049010a4a661a1e31b5
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-04 18:30:45 +0300

    Update ping.js

--- prefixcmds/developer/ping.js
+++ prefixcmds/developer/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
 	run: async (client, message) => {
 
-  if (message.author.id !== client.settings.config.owner) return;
+  if (message.author.id !== client.settings.config.ownerId) return;
 
 	await message.reply({content: `:ping_pong: Bot Gecikmesi: ${client.ws.ping} ms\n:speech_balloon: Mesaj Gecikmesi: ${new Date().getTime() - message.createdTimestamp} ms`, allowedMentions: {repliedUser: false}});
 
