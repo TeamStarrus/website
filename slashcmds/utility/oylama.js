6e1a894808f824a32a2f4cda86876d9821780c12
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 19:27:09 +0300

    Update oylama.js

--- slashcmds/utility/oylama.js
+++ slashcmds/utility/oylama.js
@@ -2,32 +2,14 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
 	data: new SlashCommandBuilder()
-  .setName(`oylama`)
-  .setDescription(`Sunucu içerisinde bir oylama/anket başlatırsınız.`)
-  .addStringOption(option => option
-    .setName(`soru`)
-    .setDescription(`Bir oylama/anket sorusu belirleyin.`)
-    .setRequired(true)
-  ),
-
+    .setName(`oylama`)
+    .setDescription(`Sunucu içerisinde bir oylama/anket başlatırsınız.`)
+    .addStringOption(option => option.setName(`soru`).setDescription(`Bir oylama/anket sorusu belirleyin.`).setRequired(true)),
 	run: async (client, interaction) => {
-
-	if (!interaction.member.permissions.has(`MANAGE_GUILD`)) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu komutu kullanabilmek için \`Sunucuyu Yönet\` iznine sahip olmalısın!`, ephemeral: true});
-
-  const {EmbedBuilder} = require(`discord.js`),
-  question = interaction.options.getString(`soru`);
-
-  let embed = new EmbedBuilder()
-  .setColor(`#00FFB8`)
-  .setTitle(`**Oylama başlatıldı!**`)
-  .setDescription(`${question}`);
-
-  await interaction.deleteReply();
-  await interaction.channel.send({embeds: [embed]})
-  .then(function (interaction) {
-    interaction.react(client.settings.config.emotes.greenTick);
-    interaction.react(client.settings.config.emotes.redTick);
-  });
-
+  	if (!interaction.member.permissions.has(`MANAGE_GUILD`)) return interaction.followUp({content: `${client.config.s.ticks.red} Bu komutu kullanabilmek için \`Sunucuyu Yönet\` iznine sahip olmalısın!`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
+    const {EmbedBuilder} = require(`discord.js`), question = interaction.options.getString(`soru`);
+    let embed = new EmbedBuilder().setColor(`#00FFB8`).setTitle(`**Oylama başlatıldı!**`).setDescription(`${question}`);
+    await interaction.deleteReply().catch(err => {});
+    await interaction.channel.send({embeds: [embed]}).then(function (interaction) {interaction.react(client.config.s.ticks.green); interaction.react(client.config.s.ticks.red)});
 	},
 };
