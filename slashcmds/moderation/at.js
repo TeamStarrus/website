67fe2a92c0f401ea7e271b30cfd0d1d472563a16
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 18:42:07 +0300

    Update at.js

--- slashcmds/moderation/at.js
+++ slashcmds/moderation/at.js
@@ -15,17 +15,17 @@ module.exports = {
       .setRequired(false)
     ),
   run: async (client, interaction) => {
-    if (!interaction.member.permissions.has(`KICK_MEMBERS`)) return interaction.followUp({content: `${client.config.s.ticks.red} Bu komutu kullanabilmek için \`Üyeleri At\` iznine sahip olmalısın!`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 3000)}).catch(err => {});
+    if (!interaction.member.permissions.has(`KICK_MEMBERS`)) return interaction.followUp({content: `${client.config.s.ticks.red} Bu komutu kullanabilmek için \`Üyeleri At\` iznine sahip olmalısın!`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
     const user = interaction.options.getUser(`üye`), member = interaction.guild.members.cache.get(user.id) || await interaction.guild.members.fetch(user.id).catch(err => {});
-    if (!member) return interaction.followUp({content: `${client.config.s.ticks.red} Sunucuda böyle bir üye bulunamadı.`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 3000)}).catch(err => {});
-    if (!member.kickable || member.user.id == client.user.id || interaction.member.roles.highest.position <= member.roles.highest.position) return interaction.followUp({content: `${client.config.s.ticks.red} Bu üyeyi sunucudan atamıyorum.`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 3000)}).catch(err => {});
+    if (!member) return interaction.followUp({content: `${client.config.s.ticks.red} Sunucuda böyle bir üye bulunamadı.`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
+    if (!member.kickable || member.user.id == client.user.id || interaction.member.roles.highest.position <= member.roles.highest.position) return interaction.followUp({content: `${client.config.s.ticks.red} Bu üyeyi sunucudan atamıyorum.`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
     const reason = interaction.options.getString(`sebep`);
     if (reason) {
-      await interaction.followUp({content: `${client.config.s.ticks.green} ${member.user.tag} adlı kullanıcı \`${reason}\` sebebiyle sunucudan atıldı!`}).then(sent => {setTimeout(function() {sent.delete()}, 3000)}).catch(err => {});
+      await interaction.followUp({content: `${client.config.s.ticks.green} ${member.user.tag} adlı kullanıcı \`${reason}\` sebebiyle sunucudan atıldı!`}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
       await member.user.send({content: `**\`${interaction.guild.name}\`** sunucusundan \`${reason}\` sebebiyle atıldın!`}).catch(err => {});
       await member.kick(reason).catch(err => {});
     } else {
-      await interaction.followUp({content: `${client.config.s.ticks.green} ${member.user.tag} adlı kullanıcı sunucudan atıldı! Sebep belirtilmedi.`}).then(sent => {setTimeout(function() {sent.delete()}, 3000)}).catch(err => {});
+      await interaction.followUp({content: `${client.config.s.ticks.green} ${member.user.tag} adlı kullanıcı sunucudan atıldı! Sebep belirtilmedi.`}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
       await member.user.send({content: `**\`${interaction.guild.name}\`** sunucusundan atıldın! Sebep belirtilmedi.`}).catch(err => {});
       await member.kick().catch(err => {});
     };
