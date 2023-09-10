9b26b60636b6d552c97fb854c6013c17b1888b42
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 18:46:16 +0300

    Update temizle.js

--- slashcmds/moderation/temizle.js
+++ slashcmds/moderation/temizle.js
@@ -2,24 +2,14 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
 	data: new SlashCommandBuilder()
-  .setName(`temizle`)
-  .setDescription(`Kanaldaki mesajları temizler.`)
-  .addStringOption(option => option
-    .setName(`miktar`)
-    .setDescription(`Temizlenecek mesaj sayısını girin.`)
-    .setRequired(true)
-  ),
-
+    .setName(`temizle`)
+    .setDescription(`Kanaldaki mesajları temizler.`)
+    .addStringOption(option => option.setName(`miktar`).setDescription(`Temizlenecek mesaj sayısını girin.`).setRequired(true)),
 	run: async (client, interaction) => {
-
-	if (!interaction.member.permissions.has(`MANAGE_MESSAGES`)) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu komutu kullanabilmek için \`Mesajları Yönet\` iznine sahip olmalısın!`, ephemeral: true});
-
-  const amount = interaction.options.getString(`miktar`);
-
-  if (!amount || amount == `0` || amount > 100) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Lütfen 1 ile 100 arasında bir değer gir!`, ephemeral: true});
-
-  await interaction.deleteReply();
-  await interaction.channel.bulkDelete(amount, true);
-
+  	if (!interaction.member.permissions.has(`MANAGE_MESSAGES`)) return interaction.followUp({content: `${client.config.s.ticks.red} Bu komutu kullanabilmek için \`Mesajları Yönet\` iznine sahip olmalısın!`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
+    const amount = interaction.options.getString(`miktar`);
+    if (!amount || amount == `0` || amount > 100) return interaction.followUp({content: `${client.config.s.ticks.red} Lütfen 1 ile 100 arasında bir değer gir!`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
+    await interaction.deleteReply().catch(err => {});
+    await interaction.channel.bulkDelete(amount, true).catch(err => {});
 	},
 };
