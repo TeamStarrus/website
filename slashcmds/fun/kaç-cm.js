f7afe2b94802b31378d8bdb78c13e70d15d48551
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 17:04:59 +0300

    Update kaç-cm.js

--- slashcmds/fun/kaç-cm.js
+++ slashcmds/fun/kaç-cm.js
@@ -2,21 +2,13 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`kaç-cm`)
-  .setDescription(`Sizinkinin ne kadar olduğunu söyler.`),
-
+    .setName(`kaç-cm`)
+    .setDescription(`Sizinkinin ne kadar olduğunu söyler.`),
   run: async (client, interaction) => {
-
-  const {EmbedBuilder} = require(`discord.js`);
-  let howlong = `Seninki tam **${Math.floor(Math.random() * 51)} cm**! Böyle devam.`;
-  if (interaction.member.id == `687021258273194016`) {howlong = `Seninki **ölçülemeyecek kadar uzun**! Beton yetmez.`};
-
-  let embed = new EmbedBuilder()
-  .setColor(`#00FFB8`)
-  .setTitle(`Kaç cm?`)
-  .setDescription(`${interaction.member} ${howlong}`);
-
-  await interaction.followUp({embeds: [embed]});
-
+    const {EmbedBuilder} = require(`discord.js`);
+    let nekadar = `Seninki tam **${Math.floor(Math.random() * 51)} cm**! Böyle devam.`;
+    if (interaction.member.id == client.config.s.ownerId) {nekadar = `Seninki **ölçülemeyecek kadar uzun**! Beton yetmez.`};
+    let embed = new EmbedBuilder().setColor(`#00FFB8`).setTitle(`Kaç cm?`).setDescription(`${interaction.member} ${nekadar}`);
+    await interaction.followUp({embeds: [embed]});
   },
 };
