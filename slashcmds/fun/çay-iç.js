37db920592901fede8f2899927f1dcbc1929dc22
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 17:52:45 +0300

    Update çay-iç.js

--- slashcmds/fun/çay-iç.js
+++ slashcmds/fun/çay-iç.js
@@ -2,19 +2,11 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`çay-iç`)
-  .setDescription(`Boğaz manzarası karşısında çay içersiniz.`),
-
+    .setName(`çay-iç`)
+    .setDescription(`Boğaz manzarası karşısında çay içersiniz.`),
   run: async (client, interaction) => {
-
-  const {EmbedBuilder} = require(`discord.js`);
-
-  let embed = new EmbedBuilder()
-  .setColor(`#00FFB8`)
-  .setImage(`https://i.hizliresim.com/4ey00j0.jpg`)
-  .setTitle(`${interaction.member.user.username} çay içti!`);
-
-  await interaction.followUp({embeds: [embed]});
-
+    const {EmbedBuilder} = require(`discord.js`);
+    let embed = new EmbedBuilder().setColor(`#00FFB8`).setImage(`https://i.hizliresim.com/4ey00j0.jpg`).setTitle(`${interaction.member.user.username} çay içti!`);
+    await interaction.followUp({embeds: [embed]});
   },
 };
