60207ac95d02a25ec7d1452a853d8c935dfde31d
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 17:51:49 +0300

    Update zar-at.js

--- slashcmds/fun/zar-at.js
+++ slashcmds/fun/zar-at.js
@@ -2,17 +2,10 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`zar-at`)
-  .setDescription(`Bot sizin için 6 yüzlü bir zar atar.`),
-
+    .setName(`zar-at`)
+    .setDescription(`Bot sizin için 6 yüzlü bir zar atar.`),
   run: async (client, interaction) => {
-
-  function dice() {
-    var rand = [`1`, `2`, `3`, `4`, `5`, `6`];
-    return rand[Math.floor(Math.random() * rand.length)];
-  };
-
-  await interaction.followUp({content: `:game_die: ${interaction.member}, zarın **${dice()}** çıktı!`, allowedMentions: {repliedUser: false}});
-
+    function dice() {var rand = [`1`, `2`, `3`, `4`, `5`, `6`]; return rand[Math.floor(Math.random() * rand.length)]};
+    await interaction.followUp({content: `:game_die: ${interaction.member}, zarın **${dice()}** çıktı!`});
   },
 };
