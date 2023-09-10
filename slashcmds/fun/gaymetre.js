dadfda19ad671fb8311e6dd27d582bd31cf9ba64
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 17:02:49 +0300

    Update gaymetre.js

--- slashcmds/fun/gaymetre.js
+++ slashcmds/fun/gaymetre.js
@@ -2,21 +2,13 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`gaymetre`)
-  .setDescription(`Yüzde kaç eşcinsel olduğunuzu öğrenirsiniz.`),
-
+    .setName(`gaymetre`)
+    .setDescription(`Yüzde kaç eşcinsel olduğunuzu öğrenirsiniz.`),
   run: async (client, interaction) => {
-
-  const {EmbedBuilder} = require(`discord.js`);
-  let howgay = `**%${Math.floor(Math.random() * 101)}**`;
-  if (interaction.member.id == `687021258273194016`) {howgay = `**%0**`};
-
-  let embed = new EmbedBuilder()
-  .setColor(`#00FFB8`)
-  .setTitle(`Gaymetre`)
-  .setDescription(`${interaction.member} ${howgay} gay :rainbow_flag:`);
-
-  await interaction.followUp({embeds: [embed]});
-
+    const {EmbedBuilder} = require(`discord.js`);
+    let yuzde = `**%${Math.floor(Math.random() * 101)}**`;
+    if (interaction.member.id == client.config.s.ownerId) {yuzde = `**%0**`};
+    let embed = new EmbedBuilder().setColor(`#00FFB8`).setTitle(`Gaymetre`).setDescription(`${interaction.member} ${yuzde} gay :rainbow_flag:`);
+    await interaction.followUp({embeds: [embed]});
   },
 };
