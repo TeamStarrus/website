772ed6f48694ff9783065f75fb52e665c907abf2
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 18:49:10 +0300

    Update söyle.js

--- slashcmds/fun/söyle.js
+++ slashcmds/fun/söyle.js
@@ -4,11 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName(`söyle`)
     .setDescription(`Bota istediğiniz mesajı söyletebilirsiniz.`)
-    .addStringOption(option => option
-      .setName(`yazı`)
-      .setDescription(`Botun söylemesini istediğiniz mesajı yazın.`)
-      .setRequired(true)
-    ),
+    .addStringOption(option => option.setName(`yazı`).setDescription(`Botun söylemesini istediğiniz mesajı yazın.`).setRequired(true)),
   run: async (client, interaction) => {
     if (!interaction.member.permissions.has(`MANAGE_MESSAGES`)) return interaction.followUp({content: `${client.config.s.ticks.red} Bu komutu kullanabilmek için \`Mesajları Yönet\` iznine sahip olmalısın!`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 3000)}).catch(err => {});
     const text = interaction.options.getString(`yazı`); await interaction.deleteReply().catch(err => {});
