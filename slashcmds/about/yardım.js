7cc30fed19fbe926f71b7c5c2362d702779ef5a4
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 16:50:57 +0300

    Update yardım.js

--- slashcmds/about/yardım.js
+++ slashcmds/about/yardım.js
@@ -2,22 +2,11 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
 	data: new SlashCommandBuilder()
-  .setName(`yardım`)
-  .setDescription(`Yardıma ihtiyacınız varsa bu komutu kullanın.`),
-
+    .setName(`yardım`)
+    .setDescription(`Yardıma ihtiyacınız varsa bu komutu kullanın.`),
 	run: async (client, interaction) => {
-
-  const {ActionRowBuilder, ButtonBuilder, ButtonStyle} = require(`discord.js`);
-
-  let row = new ActionRowBuilder()
-  .addComponents(
-    new ButtonBuilder()
-    .setStyle(ButtonStyle.Link)
-    .setLabel(`Destek sunucusuna katıl!`)
-    .setURL(`https://discord.com/invite/${client.settings.config.server}`)
-  );
-
-	await interaction.followUp({content: `${client.user.username} ile ilgili bir sorunuz veya sorununuz varsa, komutlarda bir hata bulduysanız ya da bize bir öneride bulunmak istiyorsanız aşağıdaki butonu kullanarak destek sunucumuza katılabilirsiniz.`, components: [row]});
-
+    const {ActionRowBuilder, ButtonBuilder, ButtonStyle} = require(`discord.js`);
+    let row = new ActionRowBuilder().addComponents(new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel(`Destek sunucusuna katıl!`).setURL(`https://discord.gg/${client.config.s.guildInvite}`));
+	  await interaction.followUp({content: `${client.user.username} ile ilgili bir sorunuz veya sorununuz varsa, komutlarda bir hata bulduysanız ya da bize bir öneride bulunmak istiyorsanız aşağıdaki butonu kullanarak destek sunucumuza katılabilirsiniz.`, components: [row]});
 	},
 };
