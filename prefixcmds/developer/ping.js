14e74057dc1da6b7016e8c7a49880024047fc63a
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 19:49:38 +0300

    Update ping.js

--- prefixcmds/developer/ping.js
+++ prefixcmds/developer/ping.js
@@ -1,8 +1,6 @@
 module.exports = {
 	run: async (client, message) => {
-
-  if (message.author.id !== client.config.s.ownerId) return;
-	await message.reply({content: `:ping_pong: Bot Gecikmesi: ${client.ws.ping} ms\n:speech_balloon: Mesaj Gecikmesi: ${new Date().getTime() - message.createdTimestamp} ms`, allowedMentions: {repliedUser: false}});
-
+    if (message.author.id !== client.config.s.ownerId) return;
+	  await message.reply({content: `:ping_pong: Bot Gecikmesi: ${client.ws.ping} ms\n:speech_balloon: Mesaj Gecikmesi: ${new Date().getTime() - message.createdTimestamp} ms`, allowedMentions: {repliedUser: false}});
 	},
 };
