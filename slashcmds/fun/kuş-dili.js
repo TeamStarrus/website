009429c9f0b38df0e02a25f8e99dc7c11c2546e4
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 17:27:14 +0300

    Update kuş-dili.js

--- slashcmds/fun/kuş-dili.js
+++ slashcmds/fun/kuş-dili.js
@@ -12,7 +12,7 @@ module.exports = {
   run: async (client, interaction) => {
     const text = interaction.options.getString(`yazı`);
     String.prototype.replaceAll = function (find, replace) {var str = this; return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, `\\$&`), `g`), replace)};
-    if (text.slice(0).join(` `).length <= 5) return interaction.followUp({content: `${client.config.s.ticks.red} En az 5 harf kullanmalısınız.`});
+    if (text.slice(0).join(` `).length <= 5) return interaction.followUp({content: `${client.config.s.ticks.red} En az 5 harf kullanmalısınız.`}).then(sent => {setTimeout(function() {sent.delete()}, 3000)}).catch(err => {});
     let kusdegil = text.slice(0).join(` `), kusdili = kusdegil.replaceAll(`a`, `aga`).replaceAll(`e`, `ege`).replaceAll(`i`, `igi`).replaceAll(`ı`, `igi`).replaceAll(`o`, `ogo`).replaceAll(`ö`, `ogo`).replaceAll(`u`, `ugu`).replaceAll(`ü`, `ugu`);
     await interaction.followUp({content: kusdili, allowedMentions: {repliedUser: false}});
   },
