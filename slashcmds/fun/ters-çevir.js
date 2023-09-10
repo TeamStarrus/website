7bd4ef16740a710548c999002e27a0071d2e5b95
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 18:51:54 +0300

    Update ters-çevir.js

--- slashcmds/fun/ters-çevir.js
+++ slashcmds/fun/ters-çevir.js
@@ -4,11 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName(`ters-çevir`)
     .setDescription(`Bot, yazdığınız yazıyı ters çevirir.`)
-    .addStringOption(option => option
-      .setName(`yazı`)
-      .setDescription(`Tersine çevirmek istediğiniz yazıyı yazın.`)
-      .setRequired(true)
-    ),
+    .addStringOption(option => option.setName(`yazı`).setDescription(`Tersine çevirmek istediğiniz yazıyı yazın.`).setRequired(true)),
   run: async (client, interaction) => {
     const text = interaction.options.getString(`yazı`);
     await interaction.followUp({content: text.split(``).reverse().join(``)});
