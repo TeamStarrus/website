863f55fba9dc6536eb3d528b092382ee67ca047e
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 17:29:56 +0300

    Update söyle.js

--- slashcmds/fun/söyle.js
+++ slashcmds/fun/söyle.js
@@ -2,22 +2,16 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`söyle`)
-  .setDescription(`Bota istediğiniz mesajı söyletebilirsiniz.`)
-  .addStringOption(option => option
-    .setName(`yazı`)
-    .setDescription(`Botun söylemesini istediğiniz mesajı yazın.`)
-    .setRequired(true)
-  ),
-
+    .setName(`söyle`)
+    .setDescription(`Bota istediğiniz mesajı söyletebilirsiniz.`)
+    .addStringOption(option => option
+      .setName(`yazı`)
+      .setDescription(`Botun söylemesini istediğiniz mesajı yazın.`)
+      .setRequired(true)
+    ),
   run: async (client, interaction) => {
-
-  if (!interaction.member.permissions.has(`MANAGE_MESSAGES`)) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu komutu kullanabilmek için \`Mesajları Yönet\` iznine sahip olmalısın!`, ephemeral: true});
-
-  const text = interaction.options.getString(`yazı`);
-
-  await interaction.deleteReply();
-  await interaction.channel.send({content: text, allowedMentions: {repliedUser: false}});
-
+    if (!interaction.member.permissions.has(`MANAGE_MESSAGES`)) return interaction.followUp({content: `${client.config.s.ticks.red} Bu komutu kullanabilmek için \`Mesajları Yönet\` iznine sahip olmalısın!`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 3000)}).catch(err => {});
+    const text = interaction.options.getString(`yazı`); await interaction.deleteReply().catch(err => {});
+    await interaction.channel.send({content: text, allowedMentions: {repliedUser: false}});
   },
 };
