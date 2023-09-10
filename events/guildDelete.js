371457ce5c1fc0f37ed8947a17b2e233af4e59de
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 15:53:34 +0300

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -7,7 +7,7 @@ module.exports = {
   var VerificationLevel = guild.verificationLevel, verificationLevel = (VerificationLevel == `NONE` ? (`Yok`) : (VerificationLevel == `LOW` ? (`Düşük`) : (VerificationLevel == `MEDIUM` ? (`Orta`) : (VerificationLevel == `HIGH` ? (`Yüksek`) : (VerificationLevel == `VERY_HIGH` ? (`Çok Yüksek`) : (`Bilinmiyor/Bulunamadı`))))));
   let embed = new EmbedBuilder().setColor(`#ED4245`).setTitle(`BİR SUNUCUDAN AYRILDIM`).setThumbnail(`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`).setDescription(`**Sunucu Adı:** ${guild.name}\n**Sunucu ID'si:** ${guild.id}\n\n**Sunucu Sahibi:** ${(await guild.fetchOwner()).user.tag}\n**Sunucu Sahibinin ID'si:** ${guild.ownerId}\n**Sunucu Sahibinin Profili:** <@!${guild.ownerId}>\n\n**Sunucudaki Üye Sayısı:** ${guild.members.cache.size}\n**Sunucudaki Rol Sayısı:** ${guild.roles.cache.size}\n**Sunucudaki Kanal Sayısı:** ${guild.channels.cache.size}\n**Sunucudaki Emoji Sayısı:** ${guild.emojis.cache.size}\n**Sunucudaki Çıkartma Sayısı:** ${guild.stickers.cache.size}\n\n**Doğrulama Seviyesi:** ${verificationLevel}\n**Oluşturulma Tarihi:**\n<t:${Date.parse(guild.createdAt) / 1000}:F> (<t:${Date.parse(guild.createdAt) / 1000}:R>)\n\n**Sunucu Açıklaması:** ${description}\n\n**Şu Anki Toplam Üye Sayısı:** ${client.guilds.cache.reduce((t, r) => t + r.memberCount, 0).toLocaleString()}\n**Şu Anki Toplam Kanal Sayısı:** ${client.channels.cache.size.toLocaleString()}\n**Şu Anki Toplam Sunucu Sayısı:** ${client.guilds.cache.size.toLocaleString()}`);
 
-  client.channels.cache.get(client.settings.config.logChannel).send({embeds: [embed]});
+  client.channels.cache.get(client.config.s.logChannel).send({embeds: [embed]});
 
   },
 };
