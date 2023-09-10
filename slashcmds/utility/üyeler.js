cb6b4f3a8e070727da7a985021a2cab22998ddcc
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 19:46:59 +0300

    Update üyeler.js

--- slashcmds/utility/üyeler.js
+++ slashcmds/utility/üyeler.js
@@ -2,14 +2,10 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`üyeler`)
-  .setDescription(`Sunucuda kaç üye bulunduğunu söyler.`),
-
+    .setName(`üyeler`)
+    .setDescription(`Sunucuda kaç üye bulunduğunu söyler.`),
   run: async (client, interaction) => {
-
-	if (!interaction.member.permissions.has(`MANAGE_GUILD`)) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu komutu kullanabilmek için \`Sunucuyu Yönet\` iznine sahip olmalısın!`, ephemeral: true});
-
-  await interaction.followUp({content: `:busts_in_silhouette: Bu sunucuda **${interaction.guild.memberCount}** üye bulunuyor.`});
-
+	  if (!interaction.member.permissions.has(`MANAGE_GUILD`)) return interaction.followUp({content: `${client.config.s.ticks.red} Bu komutu kullanabilmek için \`Sunucuyu Yönet\` iznine sahip olmalısın!`, ephemeral: true}).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
+    await interaction.followUp({content: `:busts_in_silhouette: Bu sunucuda **${interaction.guild.memberCount}** üye bulunuyor.`});
   },
 };
