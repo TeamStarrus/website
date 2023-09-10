a2acd0461de52e4e2b60dda9f9010bc96f279e37
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 19:50:47 +0300

    Update slashcmd.js

--- handlers/slashcmd.js
+++ handlers/slashcmd.js
@@ -2,19 +2,13 @@ const {readdirSync} = require(`fs`), ascii = require(`ascii-table`);
 let table = new ascii(`Uygulama Komutları`); table.setHeading(`Uygulama Komutu`, `Durum`);
 
 module.exports = (client) => {
-
   readdirSync(`./slashcmds`).forEach(dir => {
     const commands = readdirSync(`./slashcmds/${dir}/`).filter(file => file.endsWith(`.js`));
-    
     for (let file of commands) {
       let pull = require(`../slashcmds/${dir}/${file}`);
-
       table.addRow(file, `✅`);
-
       if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
     };
   });
-
   console.log(table.toString());
-
 };
