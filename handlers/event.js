91b5d4963d5b33132187d28550cdd0812b019be2
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 16:12:46 +0300

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,25 +1,20 @@
-const {readdirSync} = require(`fs`),
-ascii = require(`ascii-table`);
-
-let table = new ascii(`Etkinlikler`);
-table.setHeading(`Etkinlik`, `Durum`);
+const {readdirSync} = require(`fs`), ascii = require(`ascii-table`);
+let table = new ascii(`Etkinlikler`); table.setHeading(`Etkinlik`, `Durum`);
 
 module.exports = (client) => {
 
   const commands = readdirSync(`./events`).filter(file => file.endsWith(`.js`));
-  for (let file of commands) {
 
+  for (let file of commands) {
     try {
       let pull = require(`../events/${file}`);
       if (pull.event && typeof pull.event !== `string`) {
-        table.addRow(file, `❌ => Etkinlik "string" olmalıdır.`);
+        table.addRow(file, `❌ Etkinlik "string" olmalıdır.`);
         continue;
       };
-        
       pull.event = pull.event || file.replace(`.js`, ``);
       client.on(pull.event, pull.run.bind(null, client));
       table.addRow(file, `✅`);
-
     } catch (err) {
       console.log(err);
       table.addRow(file, `❌`);
