0a0c3ee78e7dc7348312266aed7afbb69e958b4a
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 19:55:42 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,15 +1,12 @@
 module.exports = {
   run: async (client) => {
-
-  client.user.setPresence({
-    activities: [{
-      name: `@${client.user.username} • /yardım • /destek`,
-      type: `WATCHING` // competing, listening, playing, streaming, watching
-    }],
-    status: `online` // dnd, idle, invisible, online
-  });
-
-  console.log(`✔️ ${client.user.tag} başarıyla aktif edildi!`);
-
+    client.user.setPresence({
+      activities: [{
+        name: `@${client.user.username} • /yardım • /destek`,
+        type: `WATCHING` // competing, listening, playing, streaming, watching
+      }],
+      status: `online` // dnd, idle, invisible, online
+    });
+    console.log(`✔️ ${client.user.tag} başarıyla aktif edildi!`);
   },
 };
