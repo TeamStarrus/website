7e3b6aab52b7b94268f6c66e99d67aa2f9bb1936
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 19:22:07 +0300

    Update hatırlat.js

--- slashcmds/utility/hatırlat.js
+++ slashcmds/utility/hatırlat.js
@@ -2,52 +2,26 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`hatırlat`)
-  .setDescription(`Sizin için hatırlatıcı oluşturur ve hatırlatır.`)
-  .addStringOption(option => option
-    .setName(`süre`)
-    .setDescription(`Süreyi girin. (Örnek: 60)`)
-    .setRequired(true)
-  )
-  .addStringOption(option => option
-    .setName(`birim`)
-    .setDescription(`Birimi girin. (Örnek: dakika)`)
-    .setRequired(true)
-  )
-  .addStringOption(option => option
-    .setName(`sebep`)
-    .setDescription(`Ne için bahsedilmek istersiniz?`)
-    .setRequired(false)
-  ),
-
+    .setName(`hatırlat`)
+    .setDescription(`Sizin için hatırlatıcı oluşturur ve hatırlatır.`)
+    .addStringOption(option => option.setName(`süre`).setDescription(`Süreyi girin. (Örnek: 60)`).setRequired(true))
+    .addStringOption(option => option.setName(`birim`).setDescription(`Birimi girin. (Örnek: dakika)`).setRequired(true))
+    .addStringOption(option => option.setName(`sebep`).setDescription(`Ne için bahsedilmek istersiniz?`).setRequired(false)),
   run: async (client, interaction) => {
-
-  const ms = require(`ms`); let unit,
-  time = interaction.options.getString(`süre`),
-  birim = interaction.options.getString(`birim`);
-
-	if (!time || time >= `60`) {
-    return interaction.followUp(`Lütfen geçerli bir süre giriniz.`);
-  };
-  if (!birim || !birim == `saniye` || !birim == `dakika` || !birim == `saat` || !birim == `gün`) { 
-    return interaction.followUp(`Lütfen geçerli bir birim giriniz.`);
-  };
-
-	if (birim == `saniye`) unit = `seconds`; if (birim == `dakika`) unit = `minutes`; if (birim == `saat`) unit = `hours`; if (birim == `gün`) unit = `days`;
-
-	return interaction.followUp(`Hatırlatıcı başarılı bir şekilde ayarlandı!`);
-
-  const reason = interaction.options.getString(`sebep`);
-  if (reason) {
-    setTimeout(function() {
-      return interaction.channel.send({content: `${interaction.member} Hatırlatıcının süresi doldu! Sebep: \`${reason}\``});
-    }, ms(`${time} ${unit}`));
-
-  } else {
-    setTimeout(function() {
-      return interaction.channel.send({content: `${interaction.member} Hatırlatıcının süresi doldu! Sebep belirtilmedi.`});
-    }, ms(`${time} ${unit}`));
-  };
-
+    const ms = require(`ms`); let unit, time = interaction.options.getString(`süre`), birim = interaction.options.getString(`birim`);
+  	if (!time || time >= `60`) {return interaction.followUp(`${client.config.s.ticks.red} Lütfen geçerli bir süre giriniz.`).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {})};
+    if (!birim || !birim == `saniye` || !birim == `dakika` || !birim == `saat` || !birim == `gün`) {return interaction.followUp(`${client.config.s.ticks.red} Lütfen geçerli bir birim giriniz.`).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {})};
+  	if (birim == `saniye`) unit = `seconds`; if (birim == `dakika`) unit = `minutes`; if (birim == `saat`) unit = `hours`; if (birim == `gün`) unit = `days`;
+	  await interaction.followUp(`${client.config.s.ticks.green} Hatırlatıcı başarılı bir şekilde ayarlandı!`).then(sent => {setTimeout(function() {sent.delete()}, 5000)}).catch(err => {});
+    const reason = interaction.options.getString(`sebep`);
+    if (reason) {
+      setTimeout(function() {
+        await interaction.channel.send({content: `${interaction.member} Hatırlatıcının süresi doldu! Sebep: \`${reason}\``});
+      }, ms(`${time} ${unit}`));
+    } else {
+      setTimeout(function() {
+        await interaction.channel.send({content: `${interaction.member} Hatırlatıcının süresi doldu! Sebep belirtilmedi.`});
+      }, ms(`${time} ${unit}`));
+    };
   },
 };
