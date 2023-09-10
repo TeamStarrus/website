4cace40e1539950a93528eb882b401d90042cdd3
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 19:50:30 +0300

    Update prefixcmd.js

--- handlers/prefixcmd.js
+++ handlers/prefixcmd.js
@@ -2,23 +2,16 @@ const {readdirSync} = require(`fs`), ascii = require(`ascii-table`);
 let table = new ascii(`Mesaj Komutları`); table.setHeading(`Mesaj Komutu`, `Durum`);
 
 module.exports = (client) => {
-
   readdirSync(`./prefixcmds`).forEach(dir => {
     const commands = readdirSync(`./prefixcmds/${dir}/`).filter(file => file.endsWith(`.js`));
-
     for (let file of commands) {
       let pull = require(`../prefixcmds/${dir}/${file}`);
-
       pull.name = file.replace(`.js`, ``);
       pull.category = dir;
-
       client.prefixcmds.set(pull.name, pull);
       table.addRow(file, `✅`);
-        
       if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
     };
   });
-
   console.log(table.toString());
-
 };
