76474753581440e3467a775b4f6472fe96c5d3ee
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 16:34:05 +0300

    Update davet.js

--- slashcmds/about/davet.js
+++ slashcmds/about/davet.js
@@ -9,13 +9,7 @@ module.exports = {
 
   const {ActionRowBuilder, ButtonBuilder, ButtonStyle} = require(`discord.js`);
 
-  let row = new ActionRowBuilder()
-  .addComponents(
-    new ButtonBuilder()
-    .setStyle(ButtonStyle.Link)
-    .setLabel(`Botu davet et!`)
-    .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&response_type=code&scope=bot+applications.commands&permissions=8`)
-  );
+  let row = new ActionRowBuilder().addComponents(new ButtonBuilder().setStyle(ButtonStyle.Link).setLabel(`Botu davet et!`).setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&response_type=code&scope=bot+applications.commands&permissions=8`));
 
 	await interaction.followUp({content: `${client.user.username} botunu kendi sunucunuza davet etmek istiyorsanız aşağıdaki butona tıklayabilirsiniz! Şimdiden teşekkür ederiz.`, components: [row]});
 
