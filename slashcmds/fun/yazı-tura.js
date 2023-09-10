f04a7026280999b23fc818d2ecce811fb20c5609
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 17:41:35 +0300

    Update yazı-tura.js

--- slashcmds/fun/yazı-tura.js
+++ slashcmds/fun/yazı-tura.js
@@ -2,23 +2,18 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`yazı-tura`)
-  .setDescription(`Bot sizin için yazı-tura atar ve sonucu söyler.`),
-
+    .setName(`yazı-tura`)
+    .setDescription(`Bot sizin için yazı-tura atar ve sonucu söyler.`),
   run: async (client, interaction) => {
-
-  const {EmbedBuilder} = require(`discord.js`);
-
-  var list = [`Yazı`, `Tura`], result = list[Math.floor(Math.random() * list.length)];
-  let coinflip = result; if (result == `Yazı`) {coinflip = `Yazı`}; if (result == `Tura`) {coinflip = `Tura`};
-  let image = coinflip; if (coinflip == `Yazı`) {image = `https://upload.wikimedia.org/wikipedia/commons/6/64/1TL_obverse.png`}; if (coinflip == `Tura`) {image = `https://upload.wikimedia.org/wikipedia/commons/c/cd/1TL_reverse.png`};
-
-  let embed = new EmbedBuilder()
-  .setColor(`#00FFB8`)
-  .setTitle(`Para atıldı. **${coinflip}** çıktı!`)
-  .setImage(image);
-
-  await interaction.followUp({embeds: [embed]});
-
+    const {EmbedBuilder} = require(`discord.js`);
+    var list = [`Yazı`, `Tura`], result = list[Math.floor(Math.random() * list.length)];
+    let coinflip = result;
+      if (result == `Yazı`) {coinflip = `Yazı`};
+      if (result == `Tura`) {coinflip = `Tura`};
+    let image = coinflip;
+      if (coinflip == `Yazı`) {image = `https://upload.wikimedia.org/wikipedia/commons/6/64/1TL_obverse.png`};
+      if (coinflip == `Tura`) {image = `https://upload.wikimedia.org/wikipedia/commons/c/cd/1TL_reverse.png`};
+    let embed = new EmbedBuilder().setColor(`#00FFB8`).setTitle(`Para atıldı. **${coinflip}** çıktı!`).setImage(image);
+    await interaction.followUp({embeds: [embed]});
   },
 };
