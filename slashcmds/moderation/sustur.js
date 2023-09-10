870dd5bf990825cc7f0326ff6339383d5d240bc9
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 18:34:26 +0300

    Update sustur.js

--- slashcmds/moderation/sustur.js
+++ slashcmds/moderation/sustur.js
@@ -2,37 +2,17 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName(`sustur`)
-  .setDescription(`Belirtilen üyeyi sunucuda susturur.`)
-  .addUserOption(option => option
-    .setName(`üye`)
-    .setDescription(`Susturmak istediğin üyeyi seç.`)
-    .setRequired(true)
-  )
-  .addStringOption(option => option
-    .setName(`süre`)
-    .setDescription(`Susturma süresini belirt. (Örnek: 60m)`)
-    .setRequired(true)
-  )
-  .addStringOption(option => option
-    .setName(`sebep`)
-    .setDescription(`Üyeyi susturma sebebini yaz.`)
-    .setRequired(false)
-  ),
-
+    .setName(`sustur`)
+    .setDescription(`Belirtilen üyeyi sunucuda susturur.`)
+    .addUserOption(option => option.setName(`üye`).setDescription(`Susturmak istediğin üyeyi seç.`).setRequired(true))
+    .addStringOption(option => option.setName(`süre`).setDescription(`Susturma süresini belirt. (Örnek: 60m)`).setRequired(true))
+    .addStringOption(option => option.setName(`sebep`).setDescription(`Üyeyi susturma sebebini yaz.`).setRequired(false)),
   run: async (client, interaction) => {
-
-  if (!interaction.member.permissions.has(`MODERATE_MEMBERS`)) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu komutu kullanabilmek için \`Üyelere Zamanaşımı Uygula\` iznine sahip olmalısın!`, ephemeral: true});
-
-  const member = interaction.options.getMember(`üye`),
-  ms = require(`ms`), time = ms(interaction.options.getString(`süre`)),
-  reason = interaction.options.getString(`sebep`) || `Sebep belirtilmedi.`;
-
-  if (!time) return interaction.followUp({content: `Lütfen geçerli bir süre giriniz.\n\`s = saniye / m = dakika / h = saat\` (Örnek: 60m)`});
-  const response = await member.timeout(time, reason);
-
-  if (!response) return interaction.followUp({content: `${client.settings.config.emotes.redTick} Bu üyeyi sunucuda susturamıyorum.`});
-  return interaction.followUp({content: `${client.settings.config.emotes.greenTick} ${member}, ${ms(time, {long: true})} süreyle susturuldu!`});
-
+    if (!interaction.member.permissions.has(`MODERATE_MEMBERS`)) return interaction.followUp({content: `${client.config.s.ticks.red} Bu komutu kullanabilmek için \`Üyelere Zamanaşımı Uygula\` iznine sahip olmalısın!`, ephemeral: true});
+    const member = interaction.options.getMember(`üye`), ms = require(`ms`), time = ms(interaction.options.getString(`süre`)), reason = interaction.options.getString(`sebep`) || `Sebep belirtilmedi.`;
+    if (!time) return interaction.followUp({content: `${client.config.s.ticks.red} Lütfen geçerli bir süre giriniz.\n\`s = saniye / m = dakika / h = saat\` (Örnek: 60m)`});
+    const response = await member.timeout(time, reason);
+    if (!response) return interaction.followUp({content: `${client.config.s.ticks.red} Bu üyeyi sunucuda susturamıyorum.`});
+    await interaction.followUp({content: `${client.config.s.ticks.green} ${member}, ${ms(time, {long: true})} süreyle susturuldu!`});
   },
 };
