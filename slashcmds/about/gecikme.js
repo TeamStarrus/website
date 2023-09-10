63cc4d508588de0ea5c539c741f65a9708345225
coanertinus <82057416+coanertinus@users.noreply.github.com>
2023-09-10 16:45:19 +0300

    Update gecikme.js

--- slashcmds/about/gecikme.js
+++ slashcmds/about/gecikme.js
@@ -2,12 +2,9 @@ const {SlashCommandBuilder} = require(`discord.js`);
 
 module.exports = {
 	data: new SlashCommandBuilder()
-  .setName(`gecikme`)
-  .setDescription(`Botun ms cinsinden gecikme süresini söyler.`),
-
+    .setName(`gecikme`)
+    .setDescription(`Botun ms cinsinden gecikme süresini söyler.`),
 	run: async (client, interaction) => {
-
-	await interaction.followUp({content: `:ping_pong: Bot Gecikmesi: ${client.ws.ping} ms\n:speech_balloon: Mesaj Gecikmesi: ${new Date().getTime() - interaction.createdTimestamp} ms`});
-
+	  await interaction.followUp({content: `:ping_pong: Bot Gecikmesi: ${client.ws.ping} ms\n:speech_balloon: Mesaj Gecikmesi: ${new Date().getTime() - interaction.createdTimestamp} ms`});
 	},
 };
