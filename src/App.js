8f563bac11005ecd21129d129436ea97ab25950b
coa <coanertinus@gmail.com>
2025-08-22 12:49:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,34 +8,29 @@ function App() {
     <div className="container">
       <div className="content">
         <BotProfile
-          imageUrl="https://cdn.discordapp.com/avatars/BOT_ID/BOT_AVATAR_HASH.png" // BOT_ID ve BOT_AVATAR_HASH kısımlarını botunuzun bilgileriyle değiştirin
+          imageUrl="https://cdn.discordapp.com/avatars/711945179535245323/625c29ea1e00e3708da5ef4ffbb55357.png"
           botName="Botunuzun Adı"
         />
 
         <p className="description">
-          Botunuzun kısa bir açıklaması veya hoş geldiniz mesajı. Bu kısım için etkileyici ve akılda kalıcı bir metin yazabilirsiniz.
+          Starrus is a fast, useful, multi-purpose and server-specific Discord.js bot with a simple coding style.
         </p>
 
         <div className="button-group">
           <LinkButton
-            text="Botu Davet Et"
-            link="https://discord.com/oauth2/authorize?client_id=1234567890&scope=bot"
-            color="#5865F2" // Discord Mavisi
+            text="Add Bot to Your Server"
+            link="https://discord.com/oauth2/authorize?client_id=711945179535245323&permissions=8&integration_type=0&response_type=code&scope=applications.commands+bot"
+            color="#5865F2"
           />
           <LinkButton
-            text="Destek Sunucusu"
-            link="https://discord.gg/desteksunucusu"
-            color="#FEE75C" // Discord Sarısı
+            text="Join Support Server"
+            link="https://discord.com/invite/nnCSmPeaGV"
+            color="#FEE75C"
           />
           <LinkButton
-            text="Web Sitesi"
-            link="https://www.ornek-site.com"
-            color="#FF5733" // Koyu Turuncu
-          />
-          <LinkButton
-            text="Kaynak Kodu"
-            link="https://github.com/kullaniciadi/bot-repo"
-            color="#99AAB5" // Koyu Gri
+            text="View Source Code"
+            link="https://github.com/coa262/StarrusDiscordBot"
+            color="#99AAB5"
           />
         </div>
       </div>
